131f444d6112415f06f55cfc30eec32c2cada393
mustafa <mserhat.uslu@bombay.io>
2025-02-22 13:32:50 +0200

    initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/projects",
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
