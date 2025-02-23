804116d2fe18f3f4b13a5b62e2e6e4c694a59975
mustafa <mserhat.uslu@bombay.io>
2025-02-23 20:14:30 +0200

    add FavprojectsSideBar

---
+++ app/_components/FavProjectsSideBar/FavProjectsSideBar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Divider, List } from "antd";
+import styled from "styled-components";
+
+const StyledList = styled(List)`
+  padding: 3rem 1rem;
+
+  .ant-list-header,
+  .ant-list-item {
+    border: none;
+    padding: 0.15rem;
+  }
+
+  .ant-list-items {
+    list-style-type: disc;
+    list-style-position: inside;
+    margin-left: 1rem;
+  }
+
+  .ant-list-item {
+    display: list-item;
+  }
+`;
+
+const data = ["Project A", "Project B", "Project C"];
+
+export default function FavProjectsSideBar() {
+  return (
+    <div className="flex">
+      <StyledList
+        size="small"
+        header={<div>Favorite Projects</div>}
+        dataSource={data}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
+      <Divider className="h-screen" type="vertical" />
+    </div>
+  );
+}
